4f9552c79301455fc61f932296e3ba6aa46b97ff
BenJR546 <162855613+BenJR546@users.noreply.github.com>
2024-07-08 22:47:32 +1000

    Array init

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@
 $('#modal-button').on('click', function() {
     $('#formModal').modal('show');
 });
+//Read the tasks from the local storage and initialize the tasks array if there are none
+function readTasksFromStorage() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (!tasks) {
+        tasks = [];
+    }
+    return tasks;
+}
